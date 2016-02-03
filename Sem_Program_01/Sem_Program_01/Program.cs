d42f4302db567f1b15d85932e90f7903ee686c7c
Alex-Pedersen <1038281@ucn.dk>
2016-02-03 12:38:11 +0100

    Oprettelse af Array af Ints samt oprettelse af sum funktion

--- Sem_Program_01/Sem_Program_01/Program.cs
+++ Sem_Program_01/Sem_Program_01/Program.cs
@@ -8,6 +8,19 @@ namespace Sem_Program_01
 {
     class Program
     {
-        
+        public int[] arrayintInts;
+
+        static void Main(string[] args)
+        {
+            var program = new Program { arrayintInts = new int[] { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 } };
+            var sum = program.SumOfArrays();
+            Console.WriteLine("Total amount of the array is : " + sum);
+
+        }
+
+        private int SumOfArrays()
+        {
+            return arrayintInts.Sum();
+        }
     }
 }
