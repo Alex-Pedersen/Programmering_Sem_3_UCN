a44c9e0e10e7e626ed8b73ea117090944fb41c66
Alex-Pedersen <1038281@ucn.dk>
2016-02-07 22:01:48 +0100

    Updating of different stuff :D

---
+++ Sem_Program_01/ConsoleApplication1/Customer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+
+    class Customer
+    {
+        public string fName, lName;
+
+        public Customer(string fName, string lName)
+        {
+            this.fName = fName;
+            this.lName = lName;
+        }
+
+        public string FName
+        {
+            get { return fName; }
+            set { fName = value; }
+        }
+
+        public string LName
+        {
+            get { return lName; }
+            set { lName = value; }
+        }
+
+        public string printCustomer(Customer customer)
+        {
+            return string.Format("Customer first and last name : {0} {1}", customer.fName, customer.lName);
+        }
+    }
+}
--- Sem_Program_01/Sem_Program_01/Program.cs
+++ Sem_Program_01/Sem_Program_01/Program.cs
@@ -9,7 +9,7 @@ namespace Sem_Program_01
 
         private static void Main()
         {
-            var program = new Program {_arrayintInts = new[] {10, 20, 30, 40, 50, 60, 70, 80, 90, 100}};
+            var program = new Program {_arrayintInts = new[] {10, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100}};
             Console.WriteLine("Array right now: {0}\n", Class1.ReturnArray(program._arrayintInts));
 
             Console.WriteLine("\nTotal amount of the array is : {0}", Class1.SumOfArray(program._arrayintInts));
@@ -25,6 +25,7 @@ namespace Sem_Program_01
             Console.WriteLine("\nEnter a number to see if it's in the Array : {0}\n", Class1.CheckerMethod(program._arrayintInts, Class1.ReadConsole()));
 
             Console.WriteLine("\n\nPress any key to close program");
+            
             Class1.ReadConsole();
         }
     }
