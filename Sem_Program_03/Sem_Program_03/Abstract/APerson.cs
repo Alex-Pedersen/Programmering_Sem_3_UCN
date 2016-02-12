608287b19068a46f6bdd4e6aec7e0e3dd3c98eb3
Alex-Pedersen <1038281@ucn.dk>
2016-02-12 15:32:27 +0100

    Making myself a solution where I make an abstract class and an interface with some simple crud functions to elaborate my understanding of their connection and usage.

---
+++ Sem_Program_03/Sem_Program_03/Abstract/APerson.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sem_Program_03.Classes;
+using Sem_Program_03.Interface;
+
+namespace Sem_Program_03.Abstract
+{
+    abstract class APerson : IPerson
+    {
+        public Person CreatePerson(string fName, string lName, string adress, int tlfNumber, int cprNumber, bool married, bool employed)
+        {
+            return new Person(fName, lName, adress, tlfNumber, cprNumber, married, employed);
+        }
+
+        public Person ReadPerson(int tlfNumber, int cprNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(string fName, string lName, string adress, int tlfNumber, int cprNumber, bool married, bool employed)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int tlfNumber, int cprNumber)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Sem_Program_03/Sem_Program_03/Interface/ICity.cs
@@ -0,0 +1,12 @@
+﻿using Sem_Program_03.Classes;
+
+namespace Sem_Program_03.Interface
+{
+    internal interface ICity
+    {
+        City CreateCity(string cityName, string cityLocation, int cityTaxAmount, Person person);
+        City ReadCity(string cityName, string cityLocation);
+        void UpdateCity(string cityName, string cityLocation, int cityTaxAmount, Person person);
+        void DeleteCity(string cityName, string cityLocation);
+    }
+}
